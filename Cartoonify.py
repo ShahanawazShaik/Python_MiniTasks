877a61aedbc4244cf1a324406c652150f3017d88
Shahanawaz <Shahanawazshaik45@gmail.com>
2021-05-27 11:49:58 +0530

    Update Cartoonify.py

--- Cartoonify.py
+++ Cartoonify.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread(r"C:\Users\noush\OneDrive\Desktop\profile.jpg")
+img = cv2.imread(r"Your image path")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray,5)
 edges = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
